32f62114d0b376a0bec471a1e6e4c57176ab85eb
JadenStanshall <jaden.uoft@gmail.com>
2025-03-10 02:50:01 -0400

    adjust comments

--- firmware/src/utils/controller.cpp
+++ firmware/src/utils/controller.cpp
@@ -1,7 +1,8 @@
 /*
 pid controller class
 
-constructor(setpoint, gains, other params)
+constructor(initial setpoints (height, pitch, roll=0), gains, other params)
 pid_correction(input measurements, output corrective control)
 release_integral()
+update_setpoints(new_height_setpoint, new_roll_setpoint)
 */
