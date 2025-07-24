74436a1c182a11487c06e1f6d48d0c06a7c8278c
Tannaz <TannazHChowdhury@gmail.com>
2025-07-24 19:14:17 -0400

    Update Sensor_Filter.c

--- Signal Filtering/Sensor_Filter.c
+++ Signal Filtering/Sensor_Filter.c
@@ -2,8 +2,6 @@
  ================================================================
  File Name: sensor_data_processor.c
  Language: C
- Author: Tannaz
- Property of: HPVDT 
  
  Description: 
  This program processes ultrasonic sensor data from text files,
