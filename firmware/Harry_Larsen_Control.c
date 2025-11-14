3049964b397e56a75a9d7ddcb109155037b564e0
Claire Nolet <119478353+ClaireNolet@users.noreply.github.com>
2025-11-13 20:45:29 -0500

    Update Harry_Larsen_Control.c
    More comments!

--- firmware/Harry_Larsen_Control.c
+++ firmware/Harry_Larsen_Control.c
@@ -1,2 +1,6 @@
 // Control equation: y = -(g1H + g2H’ + g3AoA + g4)
-// current_AOA + y = corrected_AOA 
+// control AOA: current_AOA + y = corrected_AOA 
+// H’’ = K(AoA - H/V) - g => compute H = noise - ((H''+g/k)-AOA)V and feed back into equation
+// H'' determined by taking ((current_H -prev_H -)/dt - (prev_H - two_prev_H)/dt)/dt
+// V will be defined at every point in time => V(t)
+// functions: Gaussian noise, dirac impulse, ramp function, step function
